52cdea8a9a8f13bdb59dbad57cbc675cec3f2463
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-01-11 19:47:34 -0300

    refactor: trocando parametro para utilizar id

--- src/main/java/br/com/arthur/cqrs/core/gateways/CachingService.java
+++ src/main/java/br/com/arthur/cqrs/core/gateways/CachingService.java
@@ -5,6 +5,6 @@ import br.com.arthur.cqrs.core.domain.Veiculo;
 import java.util.Optional;
 
 public interface CachingService {
-    Optional<Veiculo> get(String placa);
+    Optional<Veiculo> get(String id);
     void salva(Veiculo veiculo);
 }
