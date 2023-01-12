36d72fc4d4afe5ffc4b7abb6460ff8fb1aa06a98
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-01-12 15:53:03 -0300

    refactor: colocando nome da fila no application.properties para não utilizar hard coded

--- src/main/java/br/com/arthur/cqrs/infra/queue/RabbitMQConfig.java
+++ src/main/java/br/com/arthur/cqrs/infra/queue/RabbitMQConfig.java
@@ -2,14 +2,21 @@ package br.com.arthur.cqrs.infra.queue;
 
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    @Value("${spring.rabbitmq.queue}")
+    private String QUEUE_NAME;
 
     @Bean
     public Queue queue(){
-        return new Queue("CQRS_RABBITMQ");
+        return new Queue(QUEUE_NAME);
+    }
+
+    public String getQueueName(){
+        return QUEUE_NAME;
     }
 }
--- src/main/java/br/com/arthur/cqrs/infra/queue/RabbitMQReceiver.java
+++ src/main/java/br/com/arthur/cqrs/infra/queue/RabbitMQReceiver.java
@@ -5,16 +5,16 @@ import br.com.arthur.cqrs.core.gateways.ReadDatabase;
 import com.google.gson.Gson;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQReceiver {
-
     @Autowired
     ReadDatabase readDatabase;
 
-    @RabbitListener(queues = {"CQRS_RABBITMQ"})
+    @RabbitListener(queues = "#{rabbitMQConfig.getQueueName()}")
     public void receiveManage(@Payload String message){
         System.out.println("Mensagem do RabbitMQ recebida");
         System.out.println("processando...");
