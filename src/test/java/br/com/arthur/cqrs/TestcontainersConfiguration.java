7d31fecf0b341279d91d602863afc22d950375f7
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-04-17 15:08:53 -0300

    feat: criando teste integrado para Redis

--- src/test/java/br/com/arthur/cqrs/TestcontainersConfiguration.java
+++ src/test/java/br/com/arthur/cqrs/TestcontainersConfiguration.java
@@ -1,2 +1,21 @@
-package br.com.arthur.cqrs;public class TestContainersConfiguration {
+package br.com.arthur.cqrs;
+
+import br.com.arthur.cqrs.infra.caching.redis.RedisServiceClient;
+import java.io.File;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.DockerComposeContainer;
+
+@Configuration
+public class TestcontainersConfiguration {
+
+  public static final DockerComposeContainer<?> container =
+      new DockerComposeContainer<>(new File("docker-compose.yml"))
+          .withLocalCompose(true);
+
+  @Bean
+  public RedisServiceClient beanRedisServiceClient() {
+    return new RedisServiceClient();
+  }
+
 }
--- src/test/java/br/com/arthur/cqrs/infra/RedisServiceClientIntegrationTest.java
+++ src/test/java/br/com/arthur/cqrs/infra/RedisServiceClientIntegrationTest.java
@@ -1,5 +1,55 @@
 package br.com.arthur.cqrs.infra;
 
-public class MemcachedImplIntegrationTest {
+import br.com.arthur.cqrs.AbstractTest;
+import br.com.arthur.cqrs.TestcontainersConfiguration;
+import br.com.arthur.cqrs.core.domain.Veiculo;
+import br.com.arthur.cqrs.infra.caching.redis.RedisServiceClient;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@SpringBootTest
+@ContextConfiguration(classes = TestcontainersConfiguration.class)
+public class RedisServiceClientIntegrationTest extends AbstractTest {
+  @Autowired
+  RedisServiceClient redisClient;
+
+  @BeforeAll
+  public static void startContainers() {
+    TestcontainersConfiguration.container.start();
+  }
+
+  @AfterAll
+  public static void stopContainers() {
+    TestcontainersConfiguration.container.stop();
+  }
+
+  @Test
+  public void veiculoDeveEstarNoCacheAposSalvo(){
+    Veiculo veiculo = criaVeiculoFakeComRenavamValido();
+    veiculo.setId(ID_VEICULO);
+
+    redisClient.salva(veiculo);
+
+    Optional<Veiculo> veiculoOptional = redisClient.get(ID_VEICULO);
+
+    Assertions.assertEquals(veiculo.getId(), veiculoOptional.get().getId());
+    Assertions.assertEquals(veiculo.getMarca(), veiculoOptional.get().getMarca());
+    Assertions.assertEquals(veiculo.getModelo(), veiculoOptional.get().getModelo());
+    Assertions.assertEquals(veiculo.getAno(), veiculoOptional.get().getAno());
+    Assertions.assertEquals(veiculo.getRenavam(), veiculoOptional.get().getRenavam());
+    Assertions.assertEquals(veiculo.getPlaca(), veiculoOptional.get().getPlaca());
+    Assertions.assertEquals(veiculo.getCor(), veiculoOptional.get().getCor());
+  }
+
+  @Test
+  public void deveRetornarEmpty_quandoNaoTiverVeiculoNoCache() {
+    Optional<Veiculo> veiculoOptional = redisClient.get(ID_VEICULO);
+    Assertions.assertEquals(Optional.empty(), veiculoOptional);
+  }
 }
